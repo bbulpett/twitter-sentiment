4e4837d3a32ae501221fac8fffd435ced48ad4fb
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-01-09 15:00:42 -0500

    Include sentiment module, add sentiment route and function

--- app.js
+++ app.js
@@ -9,14 +9,45 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
 var port = 3000;
+
+// The "hello" route
 app.get('/hello', function(req, res) {
   res.send("Hello world");
 });
 
 app.listen(port);
 console.log("Server listening on port " + port);
+
+// The "testSentiment" route
+app.get('/testSentiment',
+  function (req,res) {
+    var response = "<HEAD>" +
+      "<title>Twitter Sentiment Analysis</title>\n" +
+          "</HEAD>\n" +
+          "<BODY>\n" +
+          "<P>\n" +
+          "Welcome to the Twitter Sentiment Analysis app.  " +   
+          "What phrase would you like to analzye?\n" +                
+          "</P>\n" +
+          "<FORM action=\"/testSentiment\" method=\"get\">\n" +
+          "<P>\n" +
+          "Enter a phrase to evaluate: <INPUT type=\"text\" name=\"phrase\"><BR>\n" +
+          "<INPUT type=\"submit\" value=\"Send\">\n" +
+          "</P>\n" +
+          "</FORM>\n" +
+          "</BODY>";
+    var phrase = req.query.phrase;
+    if(!phrase) {
+      res.send(response);
+    } else {
+      sentiment(phrase, function (err, result) {
+        response = 'sentiment(' + phrase + ') === ' + result.score;
+        res.send(response);
+      });
+    }
+  });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
